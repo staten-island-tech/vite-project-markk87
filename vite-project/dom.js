32cb308726d4f2ee72ed1749ee3e5d164fc6de13
markk87 <markk87@nycstudents.net>
2023-11-09 09:22:05 -0500

    sdfasd

--- vite-project/dom.js
+++ vite-project/dom.js
@@ -1,8 +1,25 @@
-const DOMSelectors = {
-    cool: document.querySelector('#coolbutton'),
-    warm: document.querySelector('#warmbutton'),
-};
 
-DOMSelectors.cool.addEventListener('click', function(event){
-    e.currentTarget.parentNode.cool();
-});
+const cold = document.getElementById("#cooler");
+
+document.querySelector(".wbut").addEventListener("click", function event() {
+  if (document.body.classList.contains("cool")) {
+
+      document.body.classList.add("warm");
+      document.body.classList.remove("cool");
+      if (cold.innerHTML == "Night") {
+        cold.innerHTML = "Morning";
+      }
+      else {
+        cold.innerHTML = "Night";
+      }
+    //   if (document.getElementById("#cooler").value="Night") {
+    //     cold.value="Morning";
+    //   }
+  } else {
+      document.body.classList.add("cool");
+      document.body.classList.remove("warm");
+      cold.value="Night";
+  }
+  
+  
+})
