efa89c3e7e4619459624e9b635bab822c7b499f8
markk87 <markk87@nycstudents.net>
2023-11-13 21:54:14 -0500

    qasedsadf

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,23 +1,27 @@
-import "../css/style.css"
+import "../css/style.css";
 import { Cars } from "./menu";
 
+var myButton = document.getElementsByTagName("button");
 
 document.querySelector(".wbut").addEventListener("click", function event() {
   if (document.body.classList.contains("cool")) {
-
-      document.body.classList.add("warm");
-      document.body.classList.remove("cool");
-      document.querySelector(".wbut").textContent = "Night"
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+    document.querySelector(".wbut").textContent = "Night";
   } else {
-      document.body.classList.add("cool");
-      document.body.classList.remove("warm");
-      document.querySelector(".wbut").textContent="Morning";
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+
+    document.querySelector(".wbut").textContent = "Morning";
+  };
+  if (myButton && myButton.classList.contains("cool-secondary")) {
+    myButton.classList.add("warm-secondary");
+    myButton.classList.remove("cool-secondary");
+  } else if (myButton) {
+    myButton.classList.add("cool-secondary");
+    myButton.classList.remove("warm-secondary");
   }
-  
-  
-})
-
-
+});
 
 console.log("Brand:", Cars[0].brand);
 console.log("Model:", Cars[0].model);
