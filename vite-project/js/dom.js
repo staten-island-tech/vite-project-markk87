da100a7c9c3fa46550ce108563718545379c826d
markk87 <markk87@nycstudents.net>
2023-11-17 09:02:10 -0500

    xcv

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -4,11 +4,14 @@ import { Cars } from "./menu";
 var DOMSelectors = {
   gallery: document.querySelector(".gallery"),
   catalog: document.querySelector(".all"),
-  luxury: document.querySelector(".luxury"),
-  sport: document.querySelector(".sport"),
+  average: document.querySelector(".average"),
+  expensive: document.querySelector(".expensive"),
   cheap: document.querySelector(".cheap"),
-  fast: document.querySelector(".fast")
 };
+
+function clearInputs(){
+  DOMSelectors.gallery.innerHTML = ''
+}
 
 document.querySelector(".wbut").addEventListener("click", function event() {
   if (document.body.classList.contains("cool")) {
@@ -23,28 +26,57 @@ document.querySelector(".wbut").addEventListener("click", function event() {
 
 });
 
-const poorPeopleCars = Cars.filter((fart)=> fart.price < 23000)
+const poorPeopleCars = Cars.filter((el)=> el.price < 23000);
+const normalPeopleCars = Cars.filter((el) => el.price > 23000 && el.price < 27000);
+const richPeopleCars = Cars.filter((el) => el.price > 35000);
+const allCars = Cars
 
-DOMSelectors.sport.addEventListener('click',function(e){
+DOMSelectors.cheap.addEventListener('click',function(e){
   e.preventDefault();
-  function fart(){
+  clearInputs();
+  function makeCard(){
     poorPeopleCars.forEach((farts)=>{
-      DOMSelectors.gallery.insertAdjacentHTML('beforeend',`<div class=card-cool><h2>${farts.brand}</h2></div>`)
+      DOMSelectors.gallery.insertAdjacentHTML('beforeend',`<div class=card-cool><h2>${farts.brand} ${farts.model}</h2></div>`)
     })
   }
-  // if (document.body.classList.contains("warm")) {
-  //   DOMSelectors.sport.classList.add("card-warm");
-  //   DOMSelectors.sport.classList.remove("card-cool");
-  // } else {
-  //   DOMSelectors.sport.classList.remove("card-warm");
-  //   DOMSelectors.sport.classList.add("card-cool");
-  // }
-  fart()
-})
+  makeCard();
+});
 
-document.querySelector(".all").addEventListener("click", function makeCardsFr() {
-  createCard();
-})
+DOMSelectors.average.addEventListener('click',function(e){
+  e.preventDefault();
+  clearInputs();
+  function makeCard(){
+    normalPeopleCars.forEach((farts)=>{
+      DOMSelectors.gallery.insertAdjacentHTML('beforeend',`<div class=card-cool><h2>${farts.brand} ${farts.model}</h2></div>`)
+    })
+  }
+  makeCard();
+});
+
+DOMSelectors.expensive.addEventListener('click',function(e){
+  e.preventDefault();
+  clearInputs();
+  function makeCard(){
+    richPeopleCars.forEach((farts)=>{
+      DOMSelectors.gallery.insertAdjacentHTML('beforeend',`<div class=card-cool><h2>${farts.brand} ${farts.model}</h2></div>`)
+    })
+  }
+  makeCard();
+});
+
+DOMSelectors.catalog.addEventListener('click',function(e){
+  e.preventDefault();
+  clearInputs();
+  function makeCard(){
+    allCars.forEach((farts)=>{
+      DOMSelectors.gallery.insertAdjacentHTML('beforeend',`<div class=card-cool><h2>${farts.brand} ${farts.model}</h2></div>`)
+    })
+  }
+  makeCard();
+});
+
+
+
 
 
 
