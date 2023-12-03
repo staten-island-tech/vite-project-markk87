8addf00613ab22bfc0a9bf146f25efc4d2993e03
markk87 <markk87@nycstudents.net>
2023-12-03 18:05:04 -0500

    asdsadf

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -9,47 +9,6 @@ const DOMSelectors = {
   cheap: document.querySelector(".cheap"),
 };
 
-
-
-
-// const deleteCard = function () {
-//   Cars.forEach((el) => el.remove());
-// }
-
-// let buttons = document.querySelectorAll("#buttons");
-
-// buttons.forEach((element) => element.addEventListener("click", function(){
-//   let filter = btn.textContent;
-//   Cars.filter((car) => car.classList.includes(filter)).forEach((c) => makeCard);
-  
-// }))
-
-function populate(Cars){
-  Cars.forEach((el) => parent.insertAdjacentHTML(
-    "beforeend",
-    `<div class='card-cool'>
-    <h2 id="name" class="name">${el.brand}</h2>
-    <img id="${el.brand}" src="${el.img} class="image">
-    <h3 id="price" class="name">${el.price}</h3>
-    </div>`
-  ))
-}
-
-function filters() {
-  let buttons = document.querySelectorAll("#buttons")
-  buttons.forEach((btn) => btn.addEventListener("click", function(){
-    let category = btn.textContent.toLowerCase()
-    let newArr = items.filter((el)=> el.price.includes(poorPeopleCars))
-    document.querySelector(".parent").innerHTML = ""
-    populate(newArr)
-  }))
-}
-
-
-function clearInputs() {
-  DOMSelectors.gallery.innerHTML = '';
-}
-
 document.querySelector(".wbut").addEventListener("click", function () {
   if (document.body.classList.contains("cool")) {
     document.body.classList.add("warm");
@@ -62,61 +21,55 @@ document.querySelector(".wbut").addEventListener("click", function () {
   }
 });
 
-const poorPeopleCars = Cars.filter((el) => el.price < 23000);
-const normalPeopleCars = Cars.filter((el) => el.price > 23000 && el.price < 27000);
-const richPeopleCars = Cars.filter((el) => el.price > 35000);
-const allCars = Cars;
 
-function makeCard(carArray) {
-  return carArray.map((car) => {
-    const card = document.createElement('div');
-    card.className = 'card-cool';
-
-    const heading = document.createElement('h2');
-    heading.className = 'card-desc';
-    heading.textContent = `${car.brand} ${car.model} : $${car.price}`;
-
-    const img = document.createElement('img');
-    img.className = 'img';
-    img.src = car.img;
-
-    card.appendChild(heading);
-    card.appendChild(img);
-
-    return card;
-  });
+function populate(parent, Cars){
+  Cars.forEach((el) => parent.insertAdjacentHTML(
+    "beforeend",
+    `<div class='card-cool'>
+    <h2 id="name" class="name">${el.brand} ${el.model}</h2>
+    <img src="${el.img} class= "img">
+    <h3 id="price" class="name">${el.price}</h3>
+    </div>`
+  ))
 }
 
-function appendCards(cards) {
-  cards.forEach((card) => {
-    DOMSelectors.gallery.appendChild(card);
-  });
+function filterButtons() {
+  const buttons = document.querySelectorAll(".buttons");
+
+  buttons.forEach((btn) => 
+    btn.addEventListener("click", function(){
+      const category = btn.textContent.toLowerCase();
+      const filteredCars = filterCars(category);
+
+      clearInputs();
+      populate(DOMSelectors.gallery, filteredCars);
+    })
+  );
 }
 
-DOMSelectors.cheap.addEventListener('click', function (e) {
-  e.preventDefault();
-  clearInputs();
-  const cards = makeCard(poorPeopleCars);
-  appendCards(cards);
-});
 
-DOMSelectors.average.addEventListener('click', function (e) {
-  e.preventDefault();
-  clearInputs();
-  const cards = makeCard(normalPeopleCars);
-  appendCards(cards);
-});
+filterButtons();
+populate(DOMSelectors.gallery, Cars);
 
-DOMSelectors.expensive.addEventListener('click', function (e) {
-  e.preventDefault();
-  clearInputs();
-  const cards = makeCard(richPeopleCars);
-  appendCards(cards);
-});
 
-DOMSelectors.catalog.addEventListener('click', function (e) {
-  e.preventDefault();
-  clearInputs();
-  const cards = makeCard(allCars);
-  appendCards(cards);
-});
+
+
+function clearInputs() {
+  DOMSelectors.gallery.innerHTML = '';
+}
+
+function filterCars(category) {
+  const filteredCars = Cars.filter((car)=> {
+    if (category === "all") {
+      return true;
+    } else if (category === "cheap") {
+      return car.price < 23000;
+    } else if (category === "average") {
+      return car.price >= 23000 && car.price < 35000;
+    } else if (category === "expensive") {
+      return car.price >= 35000;
+    }
+  });
+  return filteredCars;
+}
+
