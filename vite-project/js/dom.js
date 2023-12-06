f4a30be3c9c3cdfd145e8e75f9a9f0a326cabce8
markk87 <markk87@nycstudents.net>
2023-12-06 08:55:24 -0500

    Update dom.js

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -73,3 +73,63 @@ function filterCars(category) {
   return filteredCars;
 }
 
+
+
+// const poorPeopleCars = Cars.filter((el) => el.price < 23000);
+// const normalPeopleCars = Cars.filter((el) => el.price > 23000 && el.price < 27000);
+// const richPeopleCars = Cars.filter((el) => el.price > 35000);
+// const allCars = Cars;
+
+// function makeCard(carArray) {
+//   return carArray.map((car) => {
+//     const card = document.createElement('div');
+//     card.className = 'card-cool';
+
+//     const heading = document.createElement('h2');
+//     heading.className = 'card-desc';
+//     heading.textContent = `${car.brand} ${car.model}`;
+
+//     const img = document.createElement('img');
+//     img.className = 'img';
+//     img.src = car.img;
+
+//     card.appendChild(heading);
+//     card.appendChild(img);
+
+//     return card;
+//   });
+// }
+
+// function appendCards(cards) {
+//   cards.forEach((card) => {
+//     DOMSelectors.gallery.appendChild(card);
+//   });
+// }
+
+// DOMSelectors.cheap.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   clearInputs();
+//   const cards = makeCard(poorPeopleCars);
+//   appendCards(cards);
+// });
+
+// DOMSelectors.average.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   clearInputs();
+//   const cards = makeCard(normalPeopleCars);
+//   appendCards(cards);
+// });
+
+// DOMSelectors.expensive.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   clearInputs();
+//   const cards = makeCard(richPeopleCars);
+//   appendCards(cards);
+// });
+
+// DOMSelectors.catalog.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   clearInputs();
+//   const cards = makeCard(allCars);
+//   appendCards(cards);
+// });
